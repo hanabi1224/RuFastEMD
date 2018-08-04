401d86343af23813b28989336fe557f8657dca00
hanabi1224 <harlowmoo@gmail.com>
2018-08-03 19:09:07 -0700

    c ffi (#3)

--- tests/test.rs
+++ tests/test.rs
@@ -5,7 +5,10 @@ mod tests {
     use self::ru_fastemd::Signature;
     use self::ru_fastemd::Feature;
 
-    //use std::vec::Vec;
+    use self::ru_fastemd::fastemd as fastemd_distance_ffi;
+    use self::ru_fastemd::create_signature;
+    use self::ru_fastemd::free_signature;
+
 
     #[test]
     fn sanity_tests() {
@@ -46,9 +49,30 @@ mod tests {
         assert_eq!(dist, 160.5427069837236);
     }
 
-    /*
-    fn verify(left:&Vec<f64>, right:&Vec<f64>, expected:f64){
-        unimplemented!()
+    #[test]
+    fn c_ffi_sanity_tests(){
+        let features1_ptr = Box::into_raw(Box::from([
+            Box::into_raw(Box::from([100.0, 40.0, 22.0])) as *const f64,
+            Box::into_raw(Box::from([211.0, 20.0, 2.0]))as *const f64,
+            Box::into_raw(Box::from([32.0, 190.0, 150.0]))as *const f64,
+            Box::into_raw(Box::from([2.0, 100.0, 100.0]))as *const f64,
+        ])) as *const *const f64;
+        let weights1_ptr = Box::into_raw(Box::from([0.4, 0.3, 0.2, 0.1])) as *const f64;
+
+        let features2_ptr = Box::into_raw(Box::from([
+            Box::into_raw(Box::from([0.0, 0.0, 0.0])) as *const f64,
+            Box::into_raw(Box::from([50.0, 100.0, 80.0])) as *const f64,
+            Box::into_raw(Box::from([255.0, 255.0, 255.0])) as *const f64,
+        ])) as *const *const f64;
+        let weights2_ptr = Box::into_raw(Box::from([0.5, 0.3, 0.2])) as *const f64;
+
+        let sig1= create_signature(features1_ptr, weights1_ptr, 4, 3);
+        let sig2= create_signature(features2_ptr, weights2_ptr, 3, 3);
+
+        let dist = fastemd_distance_ffi(sig1, sig2);
+        assert_eq!(dist, 160.5427069837236);
+
+        free_signature(sig1);
+        free_signature(sig2);
     }
-    */
 }
